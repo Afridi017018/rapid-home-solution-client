3fa710b918636b756e37cb3d788b1d255985c683
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-12-07 23:59:18 +0600

    updated design of profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -81,8 +81,8 @@ const Profile = () => {
 
   return (
     <div className='px-5 lg:px-24'>
-      <div className="mx-auto m-10 p-6 bg-red-100 rounded-md">
-        <h2 className="text-2xl font-bold mb-4 text-center underline underline-offset-4 text-red-900">Profile</h2>
+      <div className="mx-auto m-10 p-6 bg-green-100 rounded-md">
+        <h2 className="text-2xl font-bold mb-4 text-center underline underline-offset-4 text-green-900">Profile</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
@@ -183,7 +183,7 @@ const Profile = () => {
           <div className='text-center'>
             <button
               type="submit"
-              className="bg-red-800 hover:bg-red-950 text-white font-bold py-2 px-10 rounded focus:outline-none focus:shadow-outline"
+              className="bg-green-800 hover:bg-green-950 text-white font-bold py-2 px-10 rounded focus:outline-none focus:shadow-outline"
             >
               Submit
             </button>
